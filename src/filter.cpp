f3d4a915bbcacf177988826876c348ae6817c1ba
jiajieli67 <nightingales.lee@gmail.com>
2019-01-25 12:07:53 +0100

    concolution by fft in progress

--- src/filter.cpp
+++ src/filter.cpp
@@ -28,3 +28,43 @@ Eigen::MatrixXd filter::convolution(const Eigen::MatrixXd &kernel,const Eigen::M
 	}
 	return mc;
 }
+
+cv::Mat filter::fft_matrix(const cv::Mat I, const cv::Mat kernel) {
+	int N1 = I.rows + kernel.rows - 1;
+	int N2 = I.cols + kernel.cols - 1;
+	cv::Mat I_padded,kernel_padded;             //expand input image to optimal size
+	cv::copyMakeBorder(I, I_padded, 0, N1 - I.rows, 0, N2 - I.cols, BORDER_CONSTANT, Scalar::all(0));
+	cv::copyMakeBorder(kernel, kernel_padded, 0, N1 - kernel.rows, 0, N2 - kernel.cols, BORDER_CONSTANT, Scalar::all(0));
+
+	cv::Mat I_efficient,kernel_efficient;
+    int m = cv::getOptimalDFTSize(N1);
+    int n = cv::getOptimalDFTSize(N2);
+    cv::copyMakeBorder(I_padded,I_efficient,0,m-I_padded.rows,0,n - I_padded.cols,BORDER_CONSTANT,Scalar::all(0));
+	cv::copyMakeBorder(kernel_padded,kernel_efficient,0,m-kernel_padded.rows,0,n - kernel_padded.cols,BORDER_CONSTANT,Scalar::all(0));
+
+	cv::Mat planesI[] = {Mat_<float>(I_padded), Mat::zeros(I_padded.size(), CV_32F)};
+    cv::Mat complexI;
+    cv::merge(planesI, 2, complexI);
+	cv::Mat planesKernel[] = {Mat_<float>(kernel_padded), Mat::zeros(kernel_padded.size(), CV_32F)};
+    cv::Mat complexKernel;
+    cv::merge(planesKernel, 2, complexKernel);
+
+	cv::dft(complexI, complexI);
+	cv::dft(complexKernel, complexKernel);
+
+	cv::split(complexI, planesI);
+	cv::split(complexKernel, planesKernel);
+
+	cv::Mat FIReal[] = {Mat_<float>(planesI[0]), Mat::zeros(planesI[0].size(), CV_32F)};
+	cv::Mat FIImag[] = {Mat_<float>(planesI[1]), Mat::zeros(planesI[1].size(), CV_32F)};
+
+	cv::Mat FkernelReal[] = {Mat_<float>(planesKernel[0]), Mat::zeros(planesKernel[0].size(), CV_32F)};
+	cv::Mat FkernelImag[] = {Mat_<float>(planesKernel[1]), Mat::zeros(planesKernel[1].size(), CV_32F)};
+
+	
+	for(int i=0;i<m;i++) {
+		for(int j=0;j<n;j++) {
+
+		}
+	}
+}
